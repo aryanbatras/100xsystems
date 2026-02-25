e9af2295bdfd51a1abb31423deb0a6f9c50d184e
aryanbatras <batraaryan03@gmail.com>
2026-02-25 18:54:54 +0530

    cache fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,10 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import Loader from "../components/loader/Loader";
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Loader />
+      <Loader /> 
       <Navbar />
       <Component {...pageProps} />
       <Footer/>
