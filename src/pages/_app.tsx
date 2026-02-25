7ba034fa9c7edca9dcc25eb318a925ac78c867a2
aryanbatras <batraaryan03@gmail.com>
2026-02-25 19:26:19 +0530

    fix error 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Loading } from "../components/loading/Loading"
 import { Navbar } from "../components/navbar/Navbar";
 import { Footer } from "../components/footer/Footer";
-import { Loading } from "../components/loader/Loading";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
