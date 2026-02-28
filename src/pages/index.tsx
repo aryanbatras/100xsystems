947c5282f3f78ee0b0a46d9e8ff93e92a1480a41
aryanbatras <batraaryan03@gmail.com>
2026-02-28 23:25:53 +0530

    added marque effect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Section7 from '../components/sections/home/Section7';
 import Section8 from '../components/sections/home/Section8';
 import Section9 from '../components/sections/home/Section9';
 import Section10 from '../components/sections/home/Section10';
+import Section11 from '../components/sections/home/Section11';
 import styles from '../components/sections/home/shared.module.css';
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <div className={styles.container}>
         <Section1 />
         <Section2 />
+        <Section11 />
         <Section3 />
         {/* <Section4 /> */}
         <Section5 />
