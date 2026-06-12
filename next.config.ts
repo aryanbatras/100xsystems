68d93765340bebb5c26c27432f079e714491f6af
batraaryan03 <aryanbatra@a2bdigital.org>
2026-06-12 15:33:25 +0530

    chore: migrate from Webpack to Turbopack and remove roughjs workaround

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,9 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  reactStrictMode: true,
-  webpack: (config: any, { isServer }: any) => {
-    // Fix for Excalidraw roughjs import issue with Webpack 5 ESM strict resolution
-    config.resolve = config.resolve || {};
-    config.resolve.alias = config.resolve.alias || {};
-    config.resolve.fullySpecified = false;
-    
-    // Add roughjs alias to fix missing .js extension issue
-    if (!isServer) {
-      config.resolve.alias['roughjs/bin/rough'] = path.resolve(
-        __dirname,
-        'node_modules/roughjs/bin/rough.js'
-      );
-    }
-    
-    return config;
-  },
+  reactStrictMode: false,
+  turbopack: {},
 };
 
 export default nextConfig;
